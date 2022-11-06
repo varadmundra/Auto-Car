2d535b4393c12d15bf7058aaa7f48f6a1b08fa20
Varad Mundra <varadmundra@gmail.com>
2022-11-06 18:36:17 +0530

    sensors

--- sensor.js
+++ sensor.js
@@ -44,7 +44,7 @@ class Sensor {
             return null;
         } else {
             const offsets = touches.map(e => e.offset);
-            const minOffset = Math.min(...offsets);
+            const minOffset = (Math.min(...offsets));
             return touches.find(e => e.minOffset);
         }
     }
