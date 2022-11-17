cbfa6968042711200e23a47b968b8f34caf984aa
Varad Mundra <varadmundra@gmail.com>
2022-11-17 16:52:19 +0530

    fo

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
-
-
-
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
 // const car = new Car(100, 100, 30, 50);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
 
 // car.draw(ctx);
-
+const traffic = [
+    new Car(road.getLaneCenter(1), -100, 30, 50)
+];
 
 animate();
 //define an animate funation
 
 //upadte the car and draw the car again 
 function animate() {
+
+    //traffic
+    for (let i = 0; i < traffic.length; i++) {
+        traffic[i].update(road.borders);
+    }
+
+
     car.update(road.borders);
 
     canvas.height = window.innerHeight;  //resizes canvas and it is clear;
@@ -25,6 +31,9 @@ function animate() {
     ctx.translate(0, -car.y + canvas.height * 0.7);
 
     road.draw(ctx);
+    for (let i = 0; i < traffic.length; i++) {
+        traffic[i].draw(ctx);
+    }
     car.draw(ctx);
 
 
